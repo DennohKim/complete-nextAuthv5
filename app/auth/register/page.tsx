925ddb9abdb7f5fe0fe8184fa3e3edf0aa33c368
Dennis Kimathi <Kimathidennis38@gmail.com>
2024-01-02 09:54:28 +0300

    create register form

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from '@/components/auth/register-form'
+import React from 'react'
+
+const Registerpage = () => {
+  return (
+	<RegisterForm />
+  )
+}
+
+export default Registerpage
