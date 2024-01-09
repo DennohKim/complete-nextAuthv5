5e85843691541722270d128436cfee665959ed6e
Dennis Kimathi <Kimathidennis38@gmail.com>
2024-01-09 11:34:56 +0300

    new password form

--- schemas/index.ts
+++ schemas/index.ts
@@ -32,3 +32,9 @@ export const ResetSchema = z.object({
   }),
  
 });
+
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: 'Minimum of 6 characters required',
+  }),
+});
