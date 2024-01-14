380f8bf3b72fe5c50d08e3fb43764348dcf45b5e
Dennis Kimathi <Kimathidennis38@gmail.com>
2024-01-14 15:03:36 +0300

    setttings page

--- auth.ts
+++ auth.ts
@@ -5,12 +5,14 @@ import { db } from '@/lib/db';
 import { getUserById } from './data/user';
 import { UserRole } from '@prisma/client';
 import { getTwoFactorConfirmationByUserId } from './data/two-factor-confirmation';
+import { getAccountByUserId } from './data/account';
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
+  update,
 } = NextAuth({
   pages: {
     signIn: '/auth/login',
@@ -31,39 +33,40 @@ export const {
   callbacks: {
     async signIn({ user, account }) {
       // allow OAuth without email verification
-	  console.log({
-		user, 
-		account
-	  })
+      console.log({
+        user,
+        account,
+      });
 
-	  //Allow OAuth without email verification
+      //Allow OAuth without email verification
       if (account?.provider !== 'credentials') return true;
 
-	  const existingUser = await getUserById(user.id);
+      const existingUser = await getUserById(user.id);
 
-	  //prevent sign in workflow if email is not verified
-	  if(!existingUser?.emailVerified) return false;
+      //prevent sign in workflow if email is not verified
+      if (!existingUser?.emailVerified) return false;
 
-	  // Add 2FA check
-	  if(existingUser?.isTwoFactorEnabled) {
-		const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
+      // Add 2FA check
+      if (existingUser?.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
 
-		console.log({
-			twoFactorConfirmation
-		});
-		
-		if(!twoFactorConfirmation) return false;
+        console.log({
+          twoFactorConfirmation,
+        });
 
-		// TODO: Delete the two factor confirmation for next sign in
-		await db.twoFactorConfirmation.delete({
-			where: {
-				id: twoFactorConfirmation.id
-			}
-		})
-	  };
-	  
+        if (!twoFactorConfirmation) return false;
 
-	  return true
+        // TODO: Delete the two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: {
+            id: twoFactorConfirmation.id,
+          },
+        });
+      }
+
+      return true;
     },
     async session({ token, session }) {
       console.log({ sessionToken: token });
@@ -78,20 +81,32 @@ export const {
         session.user.role = token.role as UserRole;
       }
 
-	   if (session.user) {
-       session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean; 
-     }
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email;
+		session.user.isOAuth = token.isOAuth as boolean; 
+      }
+
       return session;
     },
-    async jwt({ token, user }) {
+    async jwt({ token }) {
       if (!token.sub) return token;
 
       const existingUser = await getUserById(token.sub);
 
       if (!existingUser) return token;
 
+	  const existingAccount = await getAccountByUserId(existingUser.id)
+
+	  token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
-	  token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
